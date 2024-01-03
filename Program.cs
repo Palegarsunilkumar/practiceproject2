cd081e841634277ea261daee71cce4aab5adbe7f
sunil <palegar45@gmail.com>
2024-01-03 16:17:26 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceproject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader studentdata;
+            try
+            {
+                string fpath = @"C:\Users\user\Desktop\mphasis\studentdata.txt";
+                Console.WriteLine("**Student Data :**");
+                studentdata = new StreamReader(fpath);
+                string Textfile = "";
+                while ((Textfile = studentdata.ReadLine()) != null)
+                {
+                    Console.WriteLine(Textfile);
+                }
+                studentdata.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
